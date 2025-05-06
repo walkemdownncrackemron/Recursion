9339bd62b2d81d3f9a5f9b9c2882d21537784343
RON <iteginjuguna@gmail.com>
2025-05-06 16:58:00 +0300

    Recursion.py with renamed classcode.py file

--- recursion.py
+++ recursion.py
@@ -1,40 +1,56 @@
-#fib series using recursion
-def fib1(n):
-    if n==2:
-        return 1
-    if n==1:
-        return 1
-    elif n>2:
-        return fib1(n-1)+fib1(n-2)
 
-for num in range(500):
-    print(f"{num}: {fib1(num)}")
+# 1) Recursive multiplication using repeated addition
+def multix(a, b):
+    if b==0:
+        return 0
+    elif b>0:
+        return a+multix(a,b-1)
+    else:
+        return -multix(a,-b)
 
-#fib series using memoization
-def fib2(n, memo={}):
+# 2) Recursive power without using **
+def power(b, e):#e=exponent
+    if e == 0:
+        return 1
+    return b* power(b,e-1)
+
+# 3) Countdown from n to 0
+def countdown(n):
+    if n<0:
+        return print(n)
+    countdown(n - 1)
+
+# 4) Count up from 0 to n (modifying countdown)
+def countup(n, c=0): #c=current=0
+    if c>n:
+        return print(c)
+    countup(n,c+1)
+
+# 5) Reverse a string recursively
+def streverse(s):
+    if s == "":
+        return ""
+    return s[-1]+streverse(s[:-1])
+
+# 6) prime number check using recursion
+def is_prime(n, d=None):#d=divisor=None
+    if n <= 1:
+        return False
+    if d is None:
+        d=n-1
+    if d==1:
+        return True
+    if n%d==0:
+        return False
+    return is_prime(n,d-1)
+
+# 7) Recursive Fibonacci with memoization
+def fibmemo(n,memo={}):
     if n in memo:
         return memo[n]
-    if n==2:
+    if n == 0:
+        return 0
+    if n == 1:
         return 1
-    if n==1:
-        return 1
-    elif n>2:
-        memo[n]=fib2(n-1,memo)+fib2(n-2,memo)
-        return memo[n]
-
-for num in range(1001):
-    print(f"{num}: {fib2(num)}")
-
-#fib series using lru_cache
-from functools import lru_cache
-@lru_cache(maxsize=None)
-def fib3(k):
-    if k==2:
-        return 1
-    if k==1:
-        return 1
-    elif k>2:
-        return fib3(k-1)+fib3(k-2)
-
-for num in range(1001):
-    print(f"{num}: {fib3(num)}")
+    memo[n] = fibmemo(n-1,memo)+fibmemo(n-2,memo)
+    return memo[n]
