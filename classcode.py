04641e87c1a198de057b7e388ccae818bebd2cb3
RON <iteginjuguna@gmail.com>
2025-05-07 11:03:12 +0300

    Updated Tower of Hanoi

--- classcode.py
+++ classcode.py
@@ -38,3 +38,15 @@ def fib3(k):
 
 for num in range(1001):
     print(f"{num}: {fib3(num)}")
+
+#Tower of Hanoi
+def TowerOfHanoi(n, source, destination_rod, auxiliary_rod):
+    if n == 1:
+        print("Move disk 1 from source ", source, " to destination ", destination_rod)
+        return
+    TowerOfHanoi(n - 1, source, auxiliary_rod, destination_rod)
+    print("Move disk ", n, " from source ", source, " to destination ", destination_rod)
+    TowerOfHanoi(n - 1, auxiliary_rod, destination_rod, source)
+
+n = 4
+TowerOfHanoi(n, 'A', 'B', 'C')
