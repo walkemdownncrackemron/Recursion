5c392826781256b42ed728a557bb09051e15dadd
RON <iteginjuguna@gmail.com>
2025-05-07 11:21:52 +0300

    ..

--- classcode.py
+++ classcode.py
@@ -45,9 +45,9 @@ def TowerOfHanoi(n, source, destination_rod, auxiliary_rod):
     if n == 1:
         print("Move disk 1 from source ", source, " to destination ", destination_rod)
         return #base case- directly move disk 1 from source to destination when n==1
-    TowerOfHanoi(n - 1, source, auxiliary_rod, destination_rod)#recursive case- move n-1 disks from source to auxiliary rod w destination as temp rod
+    TowerOfHanoi(n - 1, source, auxiliary_rod, destination_rod)#recursive case1- move n-1 disks from source to auxiliary rod w destination as temp rod
     print("Move disk ", n, " from source ", source, " to destination ", destination_rod)# move nth disk from source to destination rod direct
-    TowerOfHanoi(n - 1, auxiliary_rod, destination_rod, source)## move n-1 disks from auxiliary rod to destination rod w source as temp rod
+    TowerOfHanoi(n - 1, auxiliary_rod, destination_rod, source)#recursive case2- move n-1 disks from auxiliary rod to destination rod w source as temp rod
 
 n = 4
 TowerOfHanoi(n, 'A', 'B', 'C')# #A,B,C are names of rods
